fc538ba316b6f76362a70e9ac83a2857ebb3630f
Larry Bonnette <larrybonnette@Larrys-Mac.local>
2018-12-24 07:15:54 -0600

    Working no button

--- BLE/ViewController.swift
+++ BLE/ViewController.swift
@@ -7,12 +7,77 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+let svcLight = CBUUID.init(string: "DF01")
+let charLightConfig = CBUUID.init(string: "DF02")
 
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            central.scanForPeripherals(withServices: nil, options: nil)
+            print ("scanning...")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if peripheral.name?.contains("ESP") == true {
+            print ("The pName is ", peripheral.name ?? "no name")
+            centralManager.stopScan()
+            print ("The Advert data is ", advertisementData)
+            central.connect(peripheral, options: nil)
+            myPeripheral = peripheral
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        central.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print ("Connected to = ", [peripheral.name])
+        peripheral.discoverServices(nil)
+        peripheral.delegate = self
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for svc in services {
+                if svc.uuid == svcLight {
+                    print ("We have found ", svc.uuid.uuidString)
+                    peripheral.discoverCharacteristics(nil, for: svc)
+                }
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let chars = service.characteristics {
+            for char in chars {
+                print (char.uuid.uuidString)
+                if char.uuid == charLightConfig {
+                    if char.properties.contains(CBCharacteristicProperties.writeWithoutResponse) {
+                        peripheral.writeValue(Data.init(bytes: [41]), for: char, type: CBCharacteristicWriteType.withoutResponse)
+                    }
+                    else {
+                        peripheral.writeValue(Data.init(bytes: [41]), for: char, type: CBCharacteristicWriteType.withResponse)
+                    }
+                }
+            }
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        print ("wrote value")
+    }
+    
+    var centralManager : CBCentralManager!
+    var myPeripheral : CBPeripheral?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager.init(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
