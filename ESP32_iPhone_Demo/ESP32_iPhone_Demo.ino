9ca4c87884961bbc6be4c8851169dca6cd550dde
bonnette <bonnette_l@yahoo.com>
2019-01-01 08:14:53 -0600

    Made comment modifications

--- ESP32_iPhone_Demo/ESP32_iPhone_Demo.ino
+++ ESP32_iPhone_Demo/ESP32_iPhone_Demo.ino
@@ -2,6 +2,10 @@
     Video: https://www.youtube.com/watch?v=oCMOYS71NIU
     Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
     Ported to Arduino ESP32 by Evandro Copercini
+    
+    Modified 1/2019 by Larry Bonnette to add communications between an iPhone and the ESP32
+    Changed service and charateristic UUID to DF01, DF02, and DF03 to make it easier to play with.
+    Added notification of LED state change sent to iPhone
 
    Create a BLE server that, once we receive a connection, will send periodic notifications.
    The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
